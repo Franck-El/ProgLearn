04f850003b1ebf9027360e1f4253bc0a112c237b
Franck <spyki095@gmail.com>
2020-05-27 12:58:47 +0200

    après lintifier

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,29 +1,29 @@
 
 async function getNumber1() {
-    return 10;
-  }
+	return 10;
+}
   
-  async function getNumber2() {
-    return 4;
-  }
+async function getNumber2() {
+	return 4;
+}
   
-  async function compute(){
-    const val1 = await getNumber1();
-    const val2 = await getNumber2();
-    return val1 + val2;
-  }
+async function compute(){
+	const val1 = await getNumber1();
+	const val2 = await getNumber2();
+	return val1 + val2;
+}
   
-  const promiseToCompute = new Promise((resolve, reject) => { 
-      resolve (compute());
-  });
+const promiseToCompute = new Promise((resolve, reject) => { 
+	resolve (compute());
+});
   
-  promiseToCompute
-    .then((value) => {
-      const result = document.getElementById("result");
-      result.textContent = value;
-    return value;
-  })
-    .then((value) => {
-      const result = document.getElementById("result");
-      result.textContent = result.textContent + ' puis ' + (value + 2);
-  });
+promiseToCompute
+	.then((value) => {
+		const result = document.getElementById("result");
+		result.textContent = value;
+		return value;
+	})
+	.then((value) => {
+		const result = document.getElementById("result");
+		result.textContent = result.textContent + " puis " + (value + 2);
+	});
